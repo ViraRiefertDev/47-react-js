448c911ea4a6fe5185c581d593bca1411050c64b
ViraRiefertDev <vira.riefert.dev@gmail.com>
2024-11-08 22:10:46 +0100

    react intro

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// импорт css из отдельного файла
+import './App.css';
+
+// функция App является React компонентом
+// в return такая функция возвращает шаблон верстки с данными
+function App() {
+   return (
+      <div className="App">
+         <h1>Hello, React!</h1>
+      </div>
+   );
+}
+
+export default App;
