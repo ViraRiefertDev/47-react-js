8864282898d4c571b0b9e248c6289ea46dae4f1b
ViraRiefertDev <vira.riefert.dev@gmail.com>
2024-11-08 22:34:56 +0100

    some changes

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+   //мы оборачиваем дочерние теги в родитель по просьбе react
+   // если нам не нужен явный контейнер мы можем воспользоваться
+   // пустым тегом - react фрагмент
    <>
       <App />
    </>
