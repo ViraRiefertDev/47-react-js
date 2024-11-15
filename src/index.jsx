e9bb40f044c0aa60a2dd7a873c8f74127b28a28b
ViraRiefertDev <vira.riefert.dev@gmail.com>
2024-11-15 06:26:11 +0100

    lesson03

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+/* import Lesson01 from './lesson01/Lesson01'; */
+//import Lesson02 from './lesson02/Lesson02';
+import Lesson03 from './lesson03/Lesson03';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   //мы оборачиваем дочерние теги в родитель по просьбе react
-   // если нам не нужен явный контейнер мы можем воспользоваться
-   // пустым тегом - react фрагмент
-   <>
-      <App />
-   </>
+  //мы оборачиваем дочерние теги в родитель по просьбе react
+  // если нам не нужен явный контейнер мы можем воспользоваться
+  // пустым тегом - react фрагмент
+  <>
+    {/* Вызов компонента происходит через обращение к нему в теге */}
+    {/* <Lesson01 /> */}
+    {/* <Lesson02 /> */}
+    <Lesson03 />
+  </>
 );
 
