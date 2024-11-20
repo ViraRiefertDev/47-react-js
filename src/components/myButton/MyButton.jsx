019f3a607d5d078783ffc229ecce93ceb23b1366
ViraRiefertDev <vira.riefert.dev@gmail.com>
2024-11-20 21:32:29 +0100

    hw4

--- src/components/myButton/MyButton.jsx
+++ src/components/myButton/MyButton.jsx
@@ -2,8 +2,8 @@ import './myButton.css';
 
 // Мы забираем переданные данные в круглых скобках
 // Обычно мы используем деструктуризацию для того, чтобы достать значения пропсов по ключам и положить их в соответстующие переменные
-function MyButton({text, onClick,type}) {
-  return <button type={type} onClick={onClick} className='myButton'>{text}</button>;
+function MyButton({text='click me!', onClick,type, isPrimary}) {
+  return <button type={type} onClick={onClick} className={`myButton ${isPrimary ? 'primary':''}`}>{text}</button>;
 }
 
 export default MyButton;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Homework04 from './homeworks/homework04/Homework04';
 /* import Lesson01 from './lesson01/Lesson01'; */
 //import Lesson02 from './lesson02/Lesson02';
 /* import Lesson03 from './lesson03/Lesson03'; */
 /* import Homework03 from './components/homework_03/Homework03'; */
-import Lesson04 from './lessons/lesson04/Lesson04';
+/* import Lesson04 from './lessons/lesson04/Lesson04'; */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,7 +19,8 @@ root.render(
     {/* <Lesson02 /> */}
     {/* <Lesson03 /> */}
     {/* <Homework03/> */}
-    <Lesson04/>
+    {/* <Lesson04/> */}
+    <Homework04/>
   </>
 );
 
